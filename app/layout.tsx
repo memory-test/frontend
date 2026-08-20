25b13caf8e2ad616612bd717be33d3c136c18820
Meladze Vladislav <mvr755@yandex.ru>
2026-08-20 16:48:58 +0300

    refactor: добавлен скрипт для запуска биом и осуществлено форматирование файлов в проекте

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <html lang="ru">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="ru">
+			<body>{children}</body>
+		</html>
+	);
 }
--- source/shared/ui/logo/logo.tsx
+++ source/shared/ui/logo/logo.tsx
@@ -4,37 +4,35 @@ import styles from "./styles.module.css";
 import type { TLogoProps } from "./types";
 
 const logoYandex = (
-  <Image
-  src="/logo-yandex.svg"
-  alt="логотип яндекса" width={48} height={48} />
+	<Image src="/logo-yandex.svg" alt="логотип яндекса" width={48} height={48} />
 );
 const logoInclusion = (
-  <Image
-    src="/logo-inclusion.svg"
-    alt="логотип инклюзии"
-    width={48}
-    height={48}
-  />
+	<Image
+		src="/logo-inclusion.svg"
+		alt="логотип инклюзии"
+		width={48}
+		height={48}
+	/>
 );
 
 const Logo = ({ urlYandex, urlInclusion }: TLogoProps) => {
-  return (
-    <div className={styles.logoContainer}>
-      {urlYandex ? (
-        <a href={urlYandex} target="_blank" rel="noopener noreferrer">
-          {logoYandex}
-        </a>
-      ) : (
-        logoYandex
-      )}
+	return (
+		<div className={styles.logoContainer}>
+			{urlYandex ? (
+				<a href={urlYandex} target="_blank" rel="noopener noreferrer">
+					{logoYandex}
+				</a>
+			) : (
+				logoYandex
+			)}
 
-      {urlInclusion ? (
-        <Link href={urlInclusion}>{logoInclusion}</Link>
-      ) : (
-        logoInclusion
-      )}
-    </div>
-  );
+			{urlInclusion ? (
+				<Link href={urlInclusion}>{logoInclusion}</Link>
+			) : (
+				logoInclusion
+			)}
+		</div>
+	);
 };
 
 export default Logo;
