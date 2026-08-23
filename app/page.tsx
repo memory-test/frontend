715c00836aaca907bf51e20cb75cd5cfc265ddaf
Annenkov-Konstantin <puerromanus@yandex.ru>
2026-08-23 22:00:00 +0300

    feat/16-button Добавлен компонент source/shared/ui/button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { QueryParamsControl } from '@features/query-params-control'
 import { createUrl, routerPath } from '@shared/lib/routes'
+import { Button } from '@shared/ui/button'
 import Link from 'next/link'
 import { Suspense } from 'react'
 
@@ -19,6 +20,38 @@ const HomePage: React.FC = () => {
 			<Suspense fallback="loading...">
 				<QueryParamsControl />
 			</Suspense>
+			<br />
+			<Button size="lg" disabled>
+				Button
+			</Button>
+			<Button size="lg">Button</Button>
+			<Button size="md">Button</Button>
+			<Button size="sm">Button</Button>
+			<br />
+			<br />
+			<Button variant="outline" size="lg" disabled>
+				Button
+			</Button>
+			<Button variant="outline" size="lg">
+				Button
+			</Button>
+			<Button variant="outline" size="md">
+				Button
+			</Button>
+			<Button variant="outline" size="sm">
+				Button
+			</Button>{' '}
+			<br />
+			<br />
+			<Button variant="ghost" size="lg" disabled>
+				Button
+			</Button>
+			<Button variant="ghost" size="lg">
+				Button
+			</Button>
+			<Button variant="ghost" size="md">
+				Button
+			</Button>
 		</div>
 	)
 }
---
+++ source/shared/ui/button/index.ts
@@ -0,0 +1,2 @@
+export { Button } from './button'
+export type { TButtonProps, TButtonSize, TButtonVariant } from './types'
