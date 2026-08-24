f20afaec7600802319bbc494768e4527821af123
lexvo <157216914+lexvo@users.noreply.github.com>
2026-08-24 22:10:11 +0300

    feat: убраны лишние контейнеры, исправлены стили и медиа-запросы

--- source/widgets/footer/footer.tsx
+++ source/widgets/footer/footer.tsx
@@ -9,34 +9,18 @@ export const Footer: React.FC<TFooterProps> = ({ children }) => {
 
 	return (
 		<footer className={styles.footer}>
-			<div className={styles.container}>
-				<div className={styles.content}>
-					<div className={styles.brand}>
-						<Logo />
-					</div>
-
-					<div className={styles.contact}>
-						<span className={styles.contactLabel}>
-							<span className={styles.contactText}>
-								Вопросы и предложения
-								<span className={styles.contactTextDesktop}> на </span>
-							</span>
-							<span className={styles.contactTextMobile}>
-								<br />
-								на{' '}
-							</span>
-							<a href={`mailto:${email}`} className={styles.contactEmail}>
-								{email}
-							</a>
-						</span>
-					</div>
-
-					{children && <div className={styles.children}>{children}</div>}
-				</div>
-
-				<div className={styles.copyright}>
-					<span>&copy; 1997 — {currentYear} ООО «Яндекс»</span>
-				</div>
+			<div className={styles.content}>
+				<Logo />
+				<p className={styles.contact}>
+					Вопросы и предложения на{' '}
+					<a href={`mailto:${email}`} className={styles.contactEmail}>
+						{email}
+					</a>
+				</p>
+				<span className={styles.copyright}>
+					&copy; 1997 — {currentYear} ООО «Яндекс»
+				</span>
+				{children && <div>{children}</div>}
 			</div>
 		</footer>
 	)
