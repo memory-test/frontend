882df2784b1ee499da3dfa8c0ee94967a925ea69
Meladze Vladislav <mvr755@yandex.ru>
2026-08-28 16:06:38 +0300

    refactor: Добавлен проп className для стилизации в месте использования

--- source/shared/ui/surface/surface.tsx
+++ source/shared/ui/surface/surface.tsx
@@ -1,6 +1,15 @@
+import clsx from 'clsx'
 import styles from './styles.module.css'
 import type { TSurfaceProps } from './types'
 
-export const Surface: React.FC<TSurfaceProps> = ({ children }) => {
-	return <div className={styles.surface}>{children}</div>
+export const Surface: React.FC<TSurfaceProps> = ({
+	children,
+	className,
+	...prop
+}) => {
+	return (
+		<div className={clsx(styles.surface, className)} {...prop}>
+			{children}
+		</div>
+	)
 }
