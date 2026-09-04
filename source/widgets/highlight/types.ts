8af99d3ec98e66c4373f0eca626c62fc85635522
lexvo <157216914+lexvo@users.noreply.github.com>
2026-09-04 12:34:44 +0300

    feat: сабтайтл, тег и экшен сделаны необязательными

--- source/widgets/highlight/types.ts
+++ source/widgets/highlight/types.ts
@@ -4,10 +4,10 @@ export type TTagColor = 'primary' | 'warning' | 'error'
 
 export type THighlightProps = {
 	title: string
-	subtitle: string
-	tagText: string
+	subtitle?: string
+	tagText?: string
 	tagColor?: TTagColor
-	actionSlot: ReactNode
+	actionSlot?: ReactNode
 	as?: 'h1' | 'h2' | 'h3' | 'h4'
 	className?: string
 }
