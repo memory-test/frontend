96a2bc534099b7d4151bd155e482b028c7d2c7e1
Maks Vovk <maksvovk259@yandex.ru>
2026-09-21 22:36:33 +0400

    feat: страница регистрации подключена к маршруту /register

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,20 @@
-const RegisterPage: React.FC = () => {
-	return <h1>Register</h1>
+'use client'
+
+import { useRegisterForm } from '@features/register'
+import { RegisterPage } from '@pages/register-page'
+
+const RegisterRoute: React.FC = () => {
+	const { register, errors, onSubmit, formError, isLoading } = useRegisterForm()
+
+	return (
+		<RegisterPage
+			register={register}
+			errors={errors}
+			onSubmit={onSubmit}
+			formError={formError}
+			isLoading={isLoading}
+		/>
+	)
 }
 
-export default RegisterPage
+export default RegisterRoute
