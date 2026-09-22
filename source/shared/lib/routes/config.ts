c09a7fb39ae2181fa3df131514fa8bc4ef3dbb2d
Maks Vovk <maksvovk259@yandex.ru>
2026-09-22 19:39:18 +0400

    feat: добавлен маршрут страницы подтверждения email

--- source/shared/lib/routes/config.ts
+++ source/shared/lib/routes/config.ts
@@ -5,6 +5,7 @@ export const routeSegments = {
 	auth: 'auth',
 	forgotPassword: 'forgot-password',
 	recoveryPassword: 'recovery-password',
+	confirm: 'confirm',
 	register: 'register',
 	profile: 'profile',
 	profileEdit: 'edit',
@@ -37,6 +38,7 @@ export const routerPath = {
 		routeSegments.auth,
 		routeSegments.recoveryPassword,
 	]),
+	confirmEmail: defineRoute([routeSegments.auth, routeSegments.confirm]),
 
 	// Остальные маршруты
 
