6faa1d8932c8aa378e518a35d4140233c71536e5
Annenkov-Konstantin <puerromanus@yandex.ru>
2026-09-23 11:23:52 +0300

    Очистил тестовую страницу

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,27 +1,11 @@
-'use client'
-
 import { QueryParamsControl } from '@features/query-params-control'
 import { createUrl, routerPath } from '@shared/lib/routes'
-import { ExerciseResult } from '@shared/ui/exercise-result'
 import Link from 'next/link'
 import { Suspense } from 'react'
-
-const MOCK_DATE_ISO = '2026-09-22T22:48:00.000Z'
 
 const HomePage: React.FC = () => {
 	return (
 		<div style={{ marginTop: '84px' }}>
-			<ExerciseResult
-				exerciseName="Запомни изображение"
-				date={MOCK_DATE_ISO} // Передаем константу
-				resultPercent={45}
-				timeSpent="00:25"
-				userAmountRightAnswer={'17'}
-				allAmountRightAnswer={'20'}
-				onReset={() => console.log('reset')}
-				onComplete={() => console.log('complete')}
-			/>
-
 			<nav>
 				<ul>
 					<li>
